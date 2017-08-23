68de2609523902f9d14f3fe6ae6c1c05236d11a6
gibbsdavidl <gibbsdavidl@gmail.com>
2017-08-23 12:55:05 -0700

    bugs follow

--- bugs/js/index.js
+++ bugs/js/index.js
@@ -1,42 +1,49 @@
 
-(function(){
 
     var canvas = document.getElementById("myCanvas");
     var ctx=canvas.getContext("2d");
 
+    var numBugs = 64;
     var bugs = [];
-    var bugCol =["#622569", "#b8a9c9", "#d6d4e0"]
     var bugLength = 3;
-    var bugSize = 5;
+    var bugSize = 8;
     var dx = 1; // how much they move per draw?
     var dy = 1;
 
+
+    function bugNumControl () {
+        var value = document.getElementById('rg').value;
+        numBugs = +value;   // + will convert the string to number
+        initBugs();
+      }
+
     function initBugs() {
         // adds to the bug list
-        // prob of bug appearing
-        var bugProb = 0.0001;  // 1% chance
-        for (x=0; x<canvas.width; x++){
-          for (y=0; y<canvas.height; y++) {
-            if (Math.random() < bugProb) {
-                // make a bug!
-                var dx = 2*(Math.random() - 0.5); // direction of travel
-                var dy = 2*(Math.random() - 0.5);
-                var xi = x; // where the bug is
-                var yi = y;
-                var bug = {'dx':dx, 'dy':dy, 'x':x, 'y':y};
-                bugs.push(bug);
-            }
-          }
+        bugs=[];
+        for (x=0; x<numBugs; x++){
+            // make a bug!
+            var dx = 2*(Math.random() - 0.5); // direction of travel
+            var dy = 2*(Math.random() - 0.5);
+            var norm = Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
+            var xi = canvas.width * Math.random(); // where the bug is
+            var yi = canvas.height * Math.random();
+            var col1 = '#'+Math.floor(Math.random()*16777215).toString(16);
+            var col2 = '#'+Math.floor(Math.random()*16777215).toString(16);
+            var col3 = '#'+Math.floor(Math.random()*16777215).toString(16);
+            var bugcol = [col1, col2, col3];
+            var bug = {'dx':dx/norm, 'dy':dy/norm, 'x':xi, 'y':yi, 'bugcol':bugcol};
+            bugs.push(bug);
         }
     }
 
     function drawBugs() {
         for(i=0; i<bugs.length; ++i) {
             r = bugs[i]
+            xc = r.bugcol;
             for (j=0; j<bugLength; j++) {
                 ctx.beginPath();
                 ctx.rect(r.x+ (-1*r.dx*j*bugSize), r.y+ (-1*r.dy*j*bugSize), bugSize, bugSize);
-                ctx.fillStyle = bugCol[j];
+                ctx.fillStyle = xc[j];
                 ctx.fill();
                 ctx.closePath();
             }
@@ -51,16 +58,44 @@
     }
 
 
-function bugsAction() {
-  // each bug can see if there's a bug near by
-  for(i=0; i<bugs.length; ++i) {
-      r = bugs[i]
-      thisx = r.x;
-      thisy = r.y;
-      // if there's a bug in this region, then course correct to follow
-
+    function dist(x1,y1,x2,y2) {
+      var d = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2));
+      return(d);
     }
-}
+
+
+    function bugsAction() {
+      // each bug can see if there's a bug near by
+      for(i=0; i<bugs.length; ++i) {
+          thisx = bugs[i].x;
+          thisy = bugs[i].y;
+          if (Math.random() < 0.01) {
+            // go crazy.
+            var bugsdx = thisx + 2*(Math.random() - 0.5); // direction of travel
+            var bugsdy = thisy + 2*(Math.random() - 0.5);
+            var norm = Math.sqrt(Math.pow(bugsdx,2) + Math.pow(bugsdy,2));
+            bugs[i].dx = bugsdx/norm;
+            bugs[i].dy = bugsdy/norm;
+          } else {
+            // if there's a bug in this region, then course correct to follow
+            for(j=0; j<bugs.length; ++j) {
+              if (i != j) {
+                thatx = bugs[j].x;
+                thaty = bugs[j].y;
+                if (dist(thisx,thisy,thatx,thaty) < 10) {
+                  // make a course correction
+                  bugsdx = bugs[i].dx + 0.2*bugs[j].dx;
+                  bugsdy = bugs[i].dy + 0.2*bugs[j].dy;
+                  norm   = Math.sqrt(Math.pow(bugsdx,2) + Math.pow(bugsdy,2));
+                  bugs[i].dx = bugsdx/norm;
+                  bugs[i].dy = bugsdy/norm;
+                }
+              }
+            }
+          }
+        }
+    }
+
 
     function draw() {        // -- //
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -75,6 +110,7 @@ function bugsAction() {
       draw();
     }
 
+(function(){
     //requestAnimationFrame(draw);
     window.onload = function() { start(); };
 })();
